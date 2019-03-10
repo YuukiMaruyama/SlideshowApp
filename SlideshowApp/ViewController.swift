604ab715460dd2022c9b660cb62b17d44f7d008f
Yuuki.Maruyama <sorachikuroneko@gmail.com>
2019-03-11 06:07:49 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by まるやまゆうき on 2019/03/11.
+//  Copyright © 2019 yuuki.maruyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
